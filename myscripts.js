596692ce2dee4834dae0a78b41cb736ed2862eec
TuanQuocDong <quoctuans2vn@tuanquocdong.computer>
2019-02-12 13:03:49 +0700

    Complete, fix some CSS to perfect

--- myscripts.js
+++ myscripts.js
@@ -31,47 +31,37 @@ function myFunction(){
    var x = document.querySelectorAll("input[type='text']");
    for (var i = 0, j = 0; i < x.length; i++, j++)
    {
-       if (x[i].value == "")
-       {
-           if (x[i] == document.getElementById("promo-code") || x[i].name == "address2")
+        if (x[i] == document.getElementById("promo-code") || x[i].name == "address2")
            {
                j--;
                continue;
            }
+        var ndiv = document.createElement("div");
+        // Add a invalid message
+        var node = document.createTextNode(mes_text[j]);
+        ndiv.appendChild(node);
+        x[i].parentNode.appendChild(ndiv);
+        ndiv.style.color = "red";
+        ndiv.style.fontSize = "1vw";
+        //x[i].setAttribute("oninput", "haveinputChange(this)");
+        x[i].addEventListener("input", haveinputChange,true);
+        x[i].addEventListener("input", noneinputChange);
+        x[i].setAttribute("onfocus", "focusChange(this)");
+        
+        x[i].addEventListener("blur", blurnoneChange);
+        x[i].addEventListener("blur", blurhaveChange);
+        if (x[i].value == "")
+        {
             x[i].style.border = "1px solid red";
-            var ndiv = document.createElement("div");
-            // Add a invalid message
-            var node = document.createTextNode(mes_text[j]);
-            ndiv.appendChild(node);
-            x[i].parentNode.appendChild(ndiv);
-            ndiv.style.color = "red";
-            ndiv.style.fontSize = "1vw";
-            //x[i].setAttribute("oninput", "haveinputChange(this)");
-            x[i].addEventListener("input", haveinputChange,true);
-            x[i].addEventListener("input", noneinputChange);
-            x[i].setAttribute("onfocus", "focusChange(this)");
-            
-            x[i].addEventListener("blur", blurnoneChange);
-            x[i].addEventListener("blur", blurhaveChange);
-       }
-       else 
-       {
-           x[i].style.border = "1px solid green";
-       }
+        }
+        else{
+            x[i].style.border = "1px solid green";
+        }
+
    }
    // For input = "email"
    var eml = document.getElementById("email");
-   if (!eml.checkValidity())
-   {
-        var ndiv = document.createElement("div");
-        // Add a invalid message
-        var node = document.createTextNode("Please enter a valid email address for shipping updates.");
-        ndiv.appendChild(node);
-        eml.parentNode.appendChild(ndiv);
-        ndiv.style.color = "red";
-        ndiv.style.fontSize = "1vw";
-        
-   }
+   email(eml);
 
    // For selected elements
    var mes_selected = [
@@ -82,22 +72,30 @@ function myFunction(){
    var slct = document.getElementsByTagName("select");
    for (var i = 0; i < slct.length; i++)
    {
-       var temp = slct[i].selectedIndex;a
-       var test = slct[i].options[temp].value;   
-       if (test == "Choose..."){
-            var ndiv = document.createElement("div");
-            var node = document.createTextNode(mes_selected[i]);
-            ndiv.appendChild(node);
-            slct[i].parentNode.appendChild(ndiv);
-            ndiv.style.color = "red";
-            ndiv.style.fontSize = "1vw";
+        var ndiv = document.createElement("div");
+        var node = document.createTextNode(mes_selected[i]);
+        ndiv.appendChild(node);
+        slct[i].parentNode.appendChild(ndiv);
+        ndiv.style.color = "red";
+        ndiv.style.fontSize = "1vw";
+        var temp = slct[i].selectedIndex;
+        var test = slct[i].options[temp].value;
+        slct[i].addEventListener("change", selectChange);   
+        if (test == "Choose..."){
+            ndiv.style.opacity = "1";
+            slct[i].style.border = "1px solid red";
+        }
+        else
+        {
+            ndiv.style.opacity = "0";
+            slct[i].style.border = "1px solid green";
         }
    }
 
 
    // For label elements
    var label = document.querySelectorAll("label");
-   for (var i = 0; i < label.length; i++)a
+   for (var i = 0; i < label.length; i++)
    {
        label[i].style.color = "green";
    }
@@ -113,7 +111,9 @@ function myFunction(){
        radiomark[i].style.border = "1px solid green";  
    }
 }
+// EVENT FUNCTION
 
+// OF intput[text]
 function haveinputChange(){
     if (this.value != "")
     {
@@ -158,9 +158,97 @@ function blurhaveChange(){
         this.setAttribute("onblur", "blurnoneChange(this)");
     }
 }
+// OF email
+function email(eml){
+    var ndiv = document.createElement("div");
+    // Add a invalid message
+    var node = document.createTextNode("Please enter a valid email address for shipping updates.");
+    ndiv.appendChild(node);
+    eml.parentNode.appendChild(ndiv);
+    ndiv.style.color = "red";
+    ndiv.style.fontSize = "1vw";
+    eml.addEventListener("input", haveemlChange,true);
+    eml.addEventListener("input", noneemlChange);
+    eml.setAttribute("onfocus", "focusemlChange(this)");
+        
+    eml.addEventListener("blur", bluremlnoneChange);
+    eml.addEventListener("blur", bluremlhaveChange);
+    if (!eml.checkValidity())
+    {
+        eml.style.border = "1px solid red";
+    }
+    else{
+        eml.style.border = "1px solid green";
+        ndiv.style.opacity = "0";
+    }
+}
+
+function haveemlChange(){
+    if (this.checkValidity())
+    {
+        focusemlChange(this);
+        var child = this.parentNode.lastChild;
+        child.style.opacity = "0";
+        var val = this;
+        val.setAttribute("oninput", "noneemlChange(this)");
+    }   
+}
+function noneemlChange(){
+    if (!this.checkValidity())
+    { 
+        focusemlChange(this);
+        var temp = this.parentNode.lastChild;
+        temp.style.opacity = "1";
+        var val = this;
+        val.setAttribute("oninput", "haveemlChange(this)");
+    }
+}
+function focusemlChange(val){
+    if (!val.checkValidity())
+    {
+        val.style.border = "3px solid lightpink"; 
+    }
+    else
+    {
+        val.style.border = "3px solid lightgreen";
+    }
+}
+function bluremlnoneChange(){
+    if (!this.checkValidity())
+    {
+        this.style.border = "1px solid red"; 
+        this.setAttribute("onblur", "bluremlhaveChange(this)");
+    }
+}
+function bluremlhaveChange(){
+    if (this.checkValidity())
+    {
+        this.style.border = "1px solid green";
+        this.setAttribute("onblur", "bluremlnoneChange(this)");
+    }
+}
+
+
+// OF address2
 function address2focus(){
     this.style.border = "3px solid lightgreen";
 }
 function address2blur(){
     this.style.border = "1px solid green";
+}
+// OF select 
+function selectChange(){
+    var val = this;
+    var test = val.options[val.selectedIndex].value;
+    var temp = val.parentNode.lastChild;
+    if (test == "Choose...")
+    {
+        temp.style.opacity = "1";
+        val.style.border = "1px solid red";
+    }
+    else
+    {
+        temp.style.opacity = "0";
+        val.style.border = "1px solid green";
+    }
 }
